068f62766106b885f08756d99de96e204f0a8de0
Everton Carmo <evertoontcarmo@gmail.com>
2021-03-04 18:59:10 -0300

    Adiciona tratamente de error ao efetuar login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,49 +3,64 @@ import { useRouter } from "next/router";
 import { FiArrowRight } from "react-icons/fi";
 import { AiFillGithub } from "react-icons/ai";
 import style from "../styles/pages/Index.module.css";
+import { ModalErrorLogin } from "../components/ModalErrorLogin";
 
 export default function Index() {
   const { push } = useRouter();
   const [username, setUsername] = useState("");
+  const [isOpenModal, setIsOpenModal] = useState(false);
 
-  function handleUsername(e) {
+  async function handleUsername(e) {
     e.preventDefault();
-    if (username) {
+
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const user = await response.json();
+
+    if (user.message != "Not Found") {
       push(`/${username}`);
+    } else {
+      setIsOpenModal(true);
     }
+  }
+  
+  function closeModal() {
+    setIsOpenModal(false)
   }
 
   return (
-    <div className={style.container}>
-      <section>
-        <div className={style.LogoSymbol}>
-          <img src="symbol.svg" alt="Simbolo Moveit" />
-        </div>
-
-        <div className={style.Gitlogin}>
-          <img src="white-logo-full.svg" alt="Logo" />
-
-          <strong>Bem-vindo</strong>
-
-          <div className={style.titleContainer}>
-            <AiFillGithub size={55} />
-            <span>Faça login com seu Github para começar</span>
+    <>
+      {isOpenModal && <ModalErrorLogin close={closeModal}/>}
+      <div className={style.container}>
+        <section>
+          <div className={style.LogoSymbol}>
+            <img src="symbol.svg" alt="Simbolo Moveit" />
           </div>
 
-          <form onSubmit={handleUsername}>
-            <input
-              type="text"
-              placeholder="Digite seu username"
-              value={username}
-              onChange={(nameUser) => setUsername(nameUser.target.value)}
-            />
-            <button type="submit">
-              {" "}
-              <FiArrowRight />{" "}
-            </button>
-          </form>
-        </div>
-      </section>
-    </div>
+          <div className={style.Gitlogin}>
+            <img src="white-logo-full.svg" alt="Logo" />
+
+            <strong>Bem-vindo</strong>
+
+            <div className={style.titleContainer}>
+              <AiFillGithub size={55} />
+              <span>Faça login com seu Github para começar</span>
+            </div>
+
+            <form onSubmit={handleUsername}>
+              <input
+                type="text"
+                placeholder="Digite seu username"
+                value={username}
+                onChange={(nameUser) => setUsername(nameUser.target.value)}
+              />
+              <button type="submit">
+                {" "}
+                <FiArrowRight />{" "}
+              </button>
+            </form>
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
