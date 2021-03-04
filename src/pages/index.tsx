179f48f2ecb404f26062aa23f4d2ed1794778bd2
Everton Carmo <evertoontcarmo@gmail.com>
2021-03-04 01:40:07 -0300

    Ajustes no layout da pagina index e no modal level up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,34 @@
 import React from "react";
-import { FiArrowRight, FiGithub } from "react-icons/fi";
+import { FiArrowRight} from "react-icons/fi";
+import {AiFillGithub} from "react-icons/ai"
 import style from "../styles/pages/Index.module.css";
 
 export default function Index() {
   return (
     <div className={style.container}>
       <section>
-        <img src="white-logo-full.svg" alt="Logo" />
-
-        <strong>Bem-vindo</strong>
-
-        <div className={style.titleContainer}>
-        <FiGithub size={55} />
-        <span>Faça login com seu Github para começar</span>
+        <div className={style.LogoSymbol}>
+          <img src="symbol.svg" alt="Simbolo Moveit" />
         </div>
 
-        <form>
-          <input type="text" placeholder="Digite seu username" />
-          <button type="submit">
-            {" "}
-            <FiArrowRight />{" "}
-          </button>
-        </form>
+        <div className={style.Gitlogin}>
+          <img src="white-logo-full.svg" alt="Logo" />
+
+          <strong>Bem-vindo</strong>
+
+          <div className={style.titleContainer}>
+            <AiFillGithub size={55} />
+            <span>Faça login com seu Github para começar</span>
+          </div>
+
+          <form>
+            <input type="text" placeholder="Digite seu username" />
+            <button type="submit">
+              {" "}
+              <FiArrowRight />{" "}
+            </button>
+          </form>
+        </div>
       </section>
     </div>
   );
